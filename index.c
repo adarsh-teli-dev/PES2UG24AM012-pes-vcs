f8ef2bc948ca34297df7ff790a6cc5545a5d6aa4
Adarsh_teli <ad.teli2005@gmail.com>
2026-04-17 22:59:59 +0530

    Phase 3: implemented atomic index_save using temp file and rename

--- index.c
+++ index.c
@@ -177,10 +177,30 @@ int index_load(Index *index) {
 //
 // Returns 0 on success, -1 on error.
 int index_save(const Index *index) {
-    // TODO: Implement atomic index saving
-    // (See Lab Appendix for logical steps)
-    (void)index;
-    return -1;
+    FILE *fp = fopen(".pes/index.tmp", "w");
+    if (!fp) return -1;
+
+    for (int i = 0; i < index->count; i++) {
+        const IndexEntry *e = &index->entries[i];
+
+        char hash_hex[65];
+        hash_to_hex(&e->hash, hash_hex);
+
+        fprintf(fp, "%o %s %ld %ld %s\n",
+                e->mode,
+                hash_hex,
+                e->mtime_sec,
+                e->size,
+                e->path);
+    }
+
+    fflush(fp);
+    fsync(fileno(fp));
+    fclose(fp);
+
+    rename(".pes/index.tmp", ".pes/index");
+
+    return 0;
 }
 
 // Stage a file for the next commit.
